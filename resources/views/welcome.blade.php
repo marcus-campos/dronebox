d72a98571a8384e4199390cec56870918bd38e2b
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-08-06 10:34:57 -0300

    Update resource

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SocialBox</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,12 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    DroneBox
+                    SocialBox
                 </div>
 
                 <div class="links">
                     <a href="http://{{env('APP_URL')}}/api/documentation">Documentation</a>
-                    <a href="https://github.com/marcus-campos/dronebox">GitHub</a>
                 </div>
             </div>
         </div>
